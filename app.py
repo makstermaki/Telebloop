5642caa0c7ad0c352143a0eff3e552996321e912
Andrew <makstermaki@gmail.com>
2020-02-09 16:45:53 -0500

    Add new absolute order column to episodes DB table to be used to determine playlist ordering Move updated date timestamp from episodes table to series table Add checks to see if series data is already loaded in DB before hitting TV Maze API

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from tendo import singleton
 
+import configparser
 import os
 import re
 import time
-import xmltv
 from pprint import pprint
 
 import common.db_utils as db_utils
@@ -16,19 +16,16 @@ me = singleton.SingleInstance()
 db_utils.initialize_db()
 
 # input_directory = '/media/TV/Steven Universe'
-input_directory = '/Volumes/TV Channels/Flying Witch'
+input_directory = '/Volumes/TV Channels/Steven Universe'
 
 
 # Given a show name, this function will populate the SQLite DB with all of the episode information
 # for that show
 def populate_tv_maze_episode_info(show_name):
-
     series_id = db_utils.get_series_id(show_name)
 
-    current_time = int(time.time())
     episodes_api_response = tv_maze.show_episode_list(series_id)
     for curr in episodes_api_response:
-        print(curr)
         episode_subtitle = 'S' + str(curr['season']) + 'E' + str(curr['number'])
         if not (curr['summary'] is None):
             description = curr['summary'].replace('<p>', '').replace('</p>', '')
@@ -36,7 +33,8 @@ def populate_tv_maze_episode_info(show_name):
             description = ''
 
         db_utils.save_tv_maze_episode_info(series_id, curr['season'], curr['number'], curr['name'],
-                                           episode_subtitle, description, current_time)
+                                           episode_subtitle, description)
+    db_utils.populate_series_absolute_order(series_id)
 
 
 def populate_episode_lengths(directory_full_path):
@@ -47,17 +45,29 @@ def populate_episode_lengths(directory_full_path):
         file_name = os.path.basename(file_full_path)
         length = playlist_utils.get_video_length(file_full_path)
         season, episode = playlist_utils.parse_season_episode(os.path.basename(file_name))
-        db_utils.save_episode_length(series_id, season, episode, length)
+        db_utils.save_local_episode_info(series_id, season, episode, length, file_full_path)
 
 
 def populate_all_episode_info(directory_full_path):
     directory_name = os.path.basename(directory_full_path)
-    populate_tv_maze_episode_info(directory_name)
-    populate_episode_lengths(directory_full_path)
+    if not db_utils.is_series_metadata_loaded(directory_name):
+        populate_tv_maze_episode_info(directory_name)
+        populate_episode_lengths(directory_full_path)
+        db_utils.update_series_last_updated_time(directory_name)
 
 
-# populate_all_episode_info(input_directory)
-
-pprint(xmltv.read_channels(open('/Users/andrew/Desktop/xmltv.xml', 'r')))
+# def start_channel(channel_name):
 
 
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# for channel in config.sections():
+#     directory = config.get(channel, "directory")
+#     order = config.get(channel, "order")
+#
+#     populate_all_episode_info(directory)
+
+result = db_utils.get_episode_info_by_season_episode(1615, 4, 10)
+print(result)
--- common/db_utils.py
+++ common/db_utils.py
@@ -1,6 +1,7 @@
 import common.tv_maze as tv_maze
 
 import sqlite3
+import time
 
 
 def connect_db():
@@ -21,44 +22,107 @@ def create_episode_table():
     c.execute('''
         CREATE TABLE episode_info (
             series_id int,
+            absolute_order int,
             season integer,
             episode integer,
             title text,
             subtitle text,
             description text,
             length real,
-            file_name text, 
-            last_updated_date integer
+            file_path text
         )
     ''')
     conn.commit()
     conn.close()
 
 
-def save_tv_maze_episode_info(series_id, season, episode, title, subtitle, desc, updated_date):
+def save_tv_maze_episode_info(series_id, season, episode, title, subtitle, desc):
     conn = connect_db()
     c = conn.cursor()
-
-    params = (series_id, season, episode, title, subtitle, desc, updated_date)
-    c.execute('INSERT INTO episode_info (series_id, season, episode, title, subtitle, description, last_updated_date) VALUES (?, ?, ?, ?, ?, ?, ?)', params)
+    params = (series_id, season, episode, title, subtitle, desc)
+    c.execute('INSERT INTO episode_info (series_id, season, episode, title, subtitle, description) VALUES (?, ?, ?, ?, ?, ?)', params)
     conn.commit()
     conn.close()
 
 
-def save_episode_length(series_id, season, episode, length):
-    print('Save Episode Length: ' + str(series_id) + ', ' + str(season) + ', ' + str(episode) + ', ' + str(length))
+def save_local_episode_info(series_id, season, episode, length, file_path):
     conn = connect_db()
     c = conn.cursor()
-
-    params = (length, series_id, season, episode,)
+    params = (length, file_path, series_id, season, episode,)
     c.execute('''
         UPDATE episode_info
-        SET length = ?
+        SET length = ?,
+            file_path = ?
         WHERE
             series_id = ? AND
             season = ? AND
             episode = ?
     ''', params)
+    conn.commit()
+    conn.close()
+
+
+def get_episode_info_by_season_episode(series_id, season, episode):
+    conn = connect_db()
+    c = conn.cursor()
+    params = (series_id, season, episode)
+    c.execute('''
+        SELECT *
+        FROM episode_info
+        WHERE
+            series_id = ? AND
+            season = ? AND
+            episode = ?
+    ''', params)
+    row = c.fetchone()
+
+    if row is not None:
+        result = {
+            'seriesID': row[0],
+            'absoluteOrder': row[1],
+            'season': row[2],
+            'episode': row[3],
+            'title': row[4],
+            'subtitle': row[5],
+            'description': row[6],
+            'length': row[7],
+            'filePath': row[8]
+        }
+    else:
+        result = None
+
+    conn.close()
+    return result
+
+
+# This function will populate the absolute order column for all episodes in a given series.
+# This order will be used to determine playback order for an in order series
+def populate_series_absolute_order(series_id):
+    conn = connect_db()
+    c = conn.cursor()
+
+    c.execute('''
+        SELECT season, episode
+        FROM episode_info
+        WHERE series_id = ?
+        ORDER BY season ASC, episode ASC
+    ''', (series_id,))
+
+    rows = c.fetchall()
+    counter = 0
+    for row in rows:
+        params = (counter, series_id, row[0], row[1],)
+        print("Counter: " + str(counter) + ", Series ID: " + str(series_id) + ", Season: " + str(row[0]) + ', Episode: ' + str(row[1]))
+        c.execute('''
+            UPDATE episode_info
+            SET absolute_order = ?
+            WHERE
+                series_id = ? AND
+                season = ? AND
+                episode = ?
+        ''', params)
+        counter = counter + 1
+
     conn.commit()
     conn.close()
 
@@ -71,7 +135,8 @@ def create_series_lookup_table():
     c.execute('''
                 CREATE TABLE series_lookup (
                     series_id int,
-                    local_series_name text
+                    local_series_name text,
+                    last_updated_date int
                 )
             ''')
     conn.commit()
@@ -94,7 +159,7 @@ def get_series_id(local_series_name):
     c = conn.cursor()
     result = c.execute('SELECT series_id FROM series_lookup WHERE local_series_name = ?', (local_series_name,))
     rows = result.fetchall()
-    c.close()
+    conn.close()
     if len(rows) == 1:
         return rows[0][0]
 
@@ -105,6 +170,33 @@ def get_series_id(local_series_name):
     return series_id
 
 
+def is_series_metadata_loaded(local_series_name):
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute('''
+        SELECT last_updated_date
+        FROM series_lookup
+        WHERE local_series_name = ?
+    ''', (local_series_name,))
+    result = c.fetchone()
+    conn.close()
+    if result is None:
+        return False
+    return True
+
+
+def update_series_last_updated_time(local_series_name):
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute('''
+        UPDATE series_lookup
+        SET last_updated_date = ?
+        WHERE local_series_name = ?
+    ''', (time.time(), local_series_name,))
+    conn.commit()
+    conn.close()
+
+
 def create_channels_table():
     conn = connect_db()
     c = conn.cursor()
@@ -112,11 +204,10 @@ def create_channels_table():
 # Channel type is sequential or random
 # Next episode season and num will be the next episode to start streaming from. (Remember, attempt to only create streams for 24 hr intervals)
     c.execute('''
-            CREATE TABLE channel_state (
+            CREATE TABLE channel (
                 channel text,
-                type text,
-                next_episode_season int,
-                next_episode_num int
+                order text,
+                next_episode int
             )
         ''')
     conn.commit()
