a5ff47477eb0465be72f2f8ff1b36d27a50dd5e9
Andrew <makstermaki@gmail.com>
2020-02-19 19:35:07 -0500

    Stop using directory name to retrieve show name Instead of using the directory name to get the show name, use the show name given in the config file Wrap the entire script in a try/catch block so no exceptions are missed for future debugging

--- app.py
+++ app.py
@@ -92,9 +92,8 @@ def populate_tv_maze_episode_info(show_name, db_dir):
     db_utils.populate_series_absolute_order(series_id, db_dir)
 
 
-def populate_episode_lengths(directory_full_path, db_dir):
-    series_directory_name = os.path.basename(directory_full_path)
-    series_id = db_utils.get_series_id(series_directory_name, db_dir)
+def populate_episode_lengths(directory_full_path, show_name, db_dir):
+    series_id = db_utils.get_series_id(show_name, db_dir)
     file_list = playlist_utils.list_files_with_path(directory_full_path)
     for file_full_path in file_list:
         file_name = os.path.basename(file_full_path)
@@ -104,12 +103,12 @@ def populate_episode_lengths(directory_full_path, db_dir):
 
 
 # Retrieves and loads all episode information given a source directory
-def populate_all_episode_info(directory_full_path, dirs):
+def populate_all_episode_info(show_name, directory_full_path, dirs):
     directory_name = os.path.basename(directory_full_path)
-    if not db_utils.is_series_metadata_loaded(directory_name, dirs['working_dir']):
-        populate_tv_maze_episode_info(directory_name, dirs['working_dir'])
-        populate_episode_lengths(directory_full_path, dirs['working_dir'])
-        db_utils.update_series_last_updated_time(directory_name, dirs['working_dir'])
+    if not db_utils.is_series_metadata_loaded(show_name, dirs['working_dir']):
+        populate_tv_maze_episode_info(show_name, dirs['working_dir'])
+        populate_episode_lengths(directory_full_path, show_name, dirs['working_dir'])
+        db_utils.update_series_last_updated_time(show_name, dirs['working_dir'])
 
 
 def start_channel(channel_name, order, shows_list, xmltv_file, dirs):
@@ -204,9 +203,6 @@ def start_channel(channel_name, order, shows_list, xmltv_file, dirs):
     concat_playlist = playlist_utils.generate_concat_playlist(playlist, dirs['playlist_dir'], channel_name)
 
     # At this point, the FFMPEG playlist has been generated so the stream can be started
-    if not os.path.exists("./pid"):
-        os.mkdir("./pid")
-
     m3u8_path = dirs['stream_dir'] + channel_name + '.m3u8'
 
     with open(dirs['log_dir'] + 'ffmpeg.log', "w") as ffmpeg_log_file:
@@ -231,132 +227,137 @@ try:
 except Exception:
     raise Exception('No config file passed in arguments!')
 
-config = configparser.ConfigParser()
-config.optionxform = str
-config.read(config_path)
+try:
 
-# Check the config file for any missing parameters and generate the default directories if so
-if config.has_option('General', 'Working Directory'):
-    working_directory = clean_directory_path(config.get('General', 'Working Directory'))
-else:
-    working_directory = default_working_dir
-    if not os.path.exists(working_directory):
-        os.mkdir(working_directory)
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config.read(config_path)
 
-log_directory = working_directory + logs_subdir
-playlist_directory = working_directory + playlist_subdir
-pid_directory = working_directory + pid_subdir
-
-if not os.path.exists(log_directory):
-    os.mkdir(log_directory)
-if not os.path.exists(playlist_directory):
-    os.mkdir(playlist_directory)
-if not os.path.exists(pid_directory):
-    os.mkdir(pid_directory)
-
-# Populate stream directory and XML TV location
-if config.has_option('General', 'Stream Directory'):
-    stream_directory = clean_directory_path(config.get('General', 'Stream Directory'))
-
-    if not os.path.exists(stream_directory):
-        os.mkdir(stream_directory)
-
-    xmltv_path = stream_directory + 'xmltv.xml'
-
-    if os.path.exists(xmltv_path):
-        file_xmltv = xmltv.open_xmltv(xmltv_path)
+    # Check the config file for any missing parameters and generate the default directories if so
+    if config.has_option('General', 'Working Directory'):
+        working_directory = clean_directory_path(config.get('General', 'Working Directory'))
     else:
-        file_xmltv = xmltv.generate_new_xmltv()
-else:
-    if not os.path.exists(default_stream_dir):
-        os.mkdir(default_stream_dir)
-    stream_directory = default_stream_dir
+        working_directory = default_working_dir
+        if not os.path.exists(working_directory):
+            os.mkdir(working_directory)
 
-    xmltv_path = default_stream_dir + 'xmltv.xml'
-    if os.path.exists(xmltv_path):
-        file_xmltv = xmltv.open_xmltv(xmltv_path)
-    else:
-        file_xmltv = xmltv.generate_new_xmltv()
+    log_directory = working_directory + logs_subdir
+    playlist_directory = working_directory + playlist_subdir
+    pid_directory = working_directory + pid_subdir
 
-if config.has_option('General', 'Log Level'):
-    logging_level = logging.getLevelName(config.get('General', 'Log Level'))
-else:
-    logging_level = logging.INFO
+    if not os.path.exists(log_directory):
+        os.mkdir(log_directory)
+    if not os.path.exists(playlist_directory):
+        os.mkdir(playlist_directory)
+    if not os.path.exists(pid_directory):
+        os.mkdir(pid_directory)
 
-# Create a dictionary for all of the working directories
-directories = {
-    'working_dir': working_directory,
-    'stream_dir': stream_directory,
-    'playlist_dir': playlist_directory,
-    'pid_dir': pid_directory,
-    'log_dir': log_directory
-}
+    # Populate stream directory and XML TV location
+    if config.has_option('General', 'Stream Directory'):
+        stream_directory = clean_directory_path(config.get('General', 'Stream Directory'))
 
-setup_logger(logging_level, directories['log_dir'])
-db_utils.initialize_db(directories['working_dir'])
+        if not os.path.exists(stream_directory):
+            os.mkdir(stream_directory)
 
-logging.info("Starting the Home Broadcaster application...")
+        xmltv_path = stream_directory + 'xmltv.xml'
 
-# Process all shows in the shows config section
-input_shows = dict(config.items('Shows'))
-for show in input_shows:
-    populate_series_info(show, directories['working_dir'])
-    populate_all_episode_info(input_shows[show], directories)
-
-
-# Stop any channels that have been removed from the config
-pid_files = playlist_utils.list_files_with_path(directories['pid_dir'])
-for pid_file_path in pid_files:
-
-    pid_channel_name = os.path.basename(pid_file_path).replace(".pid", "")
-
-    if not config.has_section(pid_channel_name):
-        old_pid_file = open(pid_file_path)
-        pid = old_pid_file.readline().strip()
-        old_pid_file.close()
-
-        clear_previous_stream_files(pid_channel_name, directories['stream_dir'], directories['playlist_dir'])
-        xmltv.remove_channel(pid_channel_name, file_xmltv)
-        xmltv.remove_channel_programmes(pid_channel_name, file_xmltv)
-        m3u.remove_channel(pid_channel_name, directories['stream_dir'])
-
-        if check_pid_running(pid):
-            logging.debug("Channel currently running but not in config. Stopping channel: " + pid_channel_name)
-            kill_running_pid(pid)
-        os.remove(pid_file_path)
-
-
-for channel in config.sections():
-    if channel == 'General' or channel == 'Shows':
-        continue
-
-    # Check if the channel is currently running and if so, do nothing
-    pid_file_path = directories['pid_dir'] + channel + '.pid'
-    if os.path.exists(pid_file_path):
-        old_pid_file = open(pid_file_path)
-        ffmpeg_pid = old_pid_file.readline().strip()
-        old_pid_file.close()
-        if check_pid_running(ffmpeg_pid):
-            # If the channel is already running, nothing needs to be done
-            logging.debug(channel + ' already running, skipping...')
-            continue
+        if os.path.exists(xmltv_path):
+            file_xmltv = xmltv.open_xmltv(xmltv_path)
         else:
-            # Channel stopped running. Clear the old stream files
-            clear_previous_stream_files(channel, directories['stream_dir'], directories['playlist_dir'])
+            file_xmltv = xmltv.generate_new_xmltv()
+    else:
+        if not os.path.exists(default_stream_dir):
+            os.mkdir(default_stream_dir)
+        stream_directory = default_stream_dir
 
-    logging.debug("Attempting to start channel: " + channel)
+        xmltv_path = default_stream_dir + 'xmltv.xml'
+        if os.path.exists(xmltv_path):
+            file_xmltv = xmltv.open_xmltv(xmltv_path)
+        else:
+            file_xmltv = xmltv.generate_new_xmltv()
 
-    # Read all of the shows to be on the channel and trim whitespaces
-    shows = config.get(channel, "Shows").split(',')
-    shows = [x.strip() for x in shows]
+    if config.has_option('General', 'Log Level'):
+        logging_level = logging.getLevelName(config.get('General', 'Log Level'))
+    else:
+        logging_level = logging.INFO
 
-    # Before attempting to start the channel, remove all previous programming for the channel from the
-    # XML TV file to avoid, overlapping programme timings
-    xmltv.remove_channel_programmes(channel, file_xmltv)
+    # Create a dictionary for all of the working directories
+    directories = {
+        'working_dir': working_directory,
+        'stream_dir': stream_directory,
+        'playlist_dir': playlist_directory,
+        'pid_dir': pid_directory,
+        'log_dir': log_directory
+    }
 
-    start_channel(channel, 'IN_ORDER', shows, file_xmltv, directories)
+    setup_logger(logging_level, directories['log_dir'])
+    db_utils.initialize_db(directories['working_dir'])
 
-    logging.debug("Finished starting channel: " + channel)
+    logging.info("Starting the Home Broadcaster application...")
 
-xmltv.remove_past_programmes(file_xmltv)
-xmltv.save_to_file(file_xmltv, xmltv_path)
+    # Process all shows in the shows config section
+    input_shows = dict(config.items('Shows'))
+    for show in input_shows:
+        populate_series_info(show, directories['working_dir'])
+        populate_all_episode_info(show, input_shows[show], directories)
+
+
+    # Stop any channels that have been removed from the config
+    pid_files = playlist_utils.list_files_with_path(directories['pid_dir'])
+    for pid_file_path in pid_files:
+
+        pid_channel_name = os.path.basename(pid_file_path).replace(".pid", "")
+
+        if not config.has_section(pid_channel_name):
+            old_pid_file = open(pid_file_path)
+            pid = old_pid_file.readline().strip()
+            old_pid_file.close()
+
+            clear_previous_stream_files(pid_channel_name, directories['stream_dir'], directories['playlist_dir'])
+            xmltv.remove_channel(pid_channel_name, file_xmltv)
+            xmltv.remove_channel_programmes(pid_channel_name, file_xmltv)
+            m3u.remove_channel(pid_channel_name, directories['stream_dir'])
+
+            if check_pid_running(pid):
+                logging.debug("Channel currently running but not in config. Stopping channel: " + pid_channel_name)
+                kill_running_pid(pid)
+            os.remove(pid_file_path)
+
+
+    for channel in config.sections():
+        if channel == 'General' or channel == 'Shows':
+            continue
+
+        # Check if the channel is currently running and if so, do nothing
+        pid_file_path = directories['pid_dir'] + channel + '.pid'
+        if os.path.exists(pid_file_path):
+            old_pid_file = open(pid_file_path)
+            ffmpeg_pid = old_pid_file.readline().strip()
+            old_pid_file.close()
+            if check_pid_running(ffmpeg_pid):
+                # If the channel is already running, nothing needs to be done
+                logging.debug(channel + ' already running, skipping...')
+                continue
+            else:
+                # Channel stopped running. Clear the old stream files
+                clear_previous_stream_files(channel, directories['stream_dir'], directories['playlist_dir'])
+
+        logging.debug("Attempting to start channel: " + channel)
+
+        # Read all of the shows to be on the channel and trim whitespaces
+        shows = config.get(channel, "Shows").split(',')
+        shows = [x.strip() for x in shows]
+
+        # Before attempting to start the channel, remove all previous programming for the channel from the
+        # XML TV file to avoid, overlapping programme timings
+        xmltv.remove_channel_programmes(channel, file_xmltv)
+
+        start_channel(channel, 'IN_ORDER', shows, file_xmltv, directories)
+
+        logging.debug("Finished starting channel: " + channel)
+
+    xmltv.remove_past_programmes(file_xmltv)
+    xmltv.save_to_file(file_xmltv, xmltv_path)
+
+except Exception:
+    logging.exception("Error occurred in script")
